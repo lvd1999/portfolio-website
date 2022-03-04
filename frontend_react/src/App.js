3b384032d06a191d24e156acb6955407f14b0d60
Patrick <patricklee0221@gmail.com>
2022-03-04 12:01:04 +0000

    import initial containers into app

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import { About, Footer, Header, Skills, Testimonials, Work } from './container';
+
 const App = () => {
-  return <div>App</div>;
+  <div className="app">
+    <Header />
+    <About />
+    <Work />
+    <Skills />
+    <Testimonials />
+    <Footer />
+  </div>;
 };
 
 export default App;
