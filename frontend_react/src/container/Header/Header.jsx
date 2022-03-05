233c2bdfb0d258c49fdfb8d8afbd8ca83e58b8ce
Patrick <patricklee0221@gmail.com>
2022-03-05 20:37:15 +0000

    details edit

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -24,10 +24,10 @@ const Header = () => (
     >
       <div className="app__header-badge">
         <div className="badge-cmp app__flex">
-          <span>👋</span>
+          <span>👋🏻</span>
           <div style={{ marginLeft: 20 }}>
             <p className="p-text">Hello, I am</p>
-            <h1 className="head-text">Micael</h1>
+            <h1 className="head-text">Lee</h1>
           </div>
         </div>
 
